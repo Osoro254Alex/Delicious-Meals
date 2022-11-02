c1e3b690bffaa95b0ab8425a30e07abd16ef2a6b
Alex Ogeto <103739438+Osoro254Alex@users.noreply.github.com>
2022-11-02 15:12:15 +0300

    Modify src/index.js

--- src/index.js
+++ src/index.js
@@ -6,23 +6,38 @@ const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=k';
 
 const involve = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/wIvcfoeCMowsKdAOdXJy/likes/';
 
+
 const liking = (arr) => {
   const ids = document.querySelectorAll('.likes');
   arr.forEach((element) => {
     ids.forEach((id) => {
       if ((element.item_id) === Number((id.dataset.id))) {
-        console.log(ids)
-        ids.innerText = `${element.likes} likes`;
+        id.innerText = `${element.likes} likes`;
       }
     });
   });
 };
+
 
 const getArr = () => {
   fetch(involve)
     .then((response) => response.json())
     .then((data) => liking(data));
 };
+
+
+cardsContainer.addEventListener("click", (e)=>{
+
+  if(e.target.className === "fa-regular fa-heart icon"){
+    const children = e.target.parentElement.children;
+    const childArr = Array.from(children)
+    childArr.forEach((child) =>{
+      child.classList.toggle("active")
+    })
+  
+  }
+  
+  })
 
 
 fetch(url)
@@ -42,45 +57,44 @@ fetch(url)
                 <i class="fa-solid fa-heart icon active"></i>
               </div>
           </div>
-          <p data-id ="${item.idMeal}" class="likes">0 Likes</p>
+          <p data-id ="${item.idMeal}" class="likes">5 Likes</p>
           <div class="btns">
               <button class="comment">Comments</button>
           </div>
         </section>`;
     });
 
-  });
+    const like = Array.from(document.querySelectorAll('.fa-regular'));
+   like.forEach((item) => {
+  item.addEventListener('click', () => {
+  let make = parseInt(item.parentElement.parentElement.nextElementSibling.innerText, 10);
+  const identit = Number(item.parentElement.id);
 
-cardsContainer.addEventListener("click", (e)=>{
+    const object = {
+      item_id: identit,
+      likes: make,
+    };
 
-if(e.target.className === "fa-regular fa-heart icon"){
-  const children = e.target.parentElement.children;
-  const childArr = Array.from(children)
-  childArr.forEach((child) =>{
-    child.classList.toggle("active")
-  })
+    fetch(involve, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(object),
+    });
 
-}
+    make += 1;
 
-if(e.target.className === "fa-regular fa-heart icon active"){
-  const identit = (Number(e.target.parentElement.id))
-  let likes = parseInt(e.target.parentElement.parentElement.nextElementSibling.innerText)
-  const object = {
-    item_id: identit,
-    likes: likes,
-  };
-  fetch(involve, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(object),
-  });
-  likes += 1;
-
-  e.target.parentElement.parentElement.nextElementSibling.innerText = `${likes} likes;`
-}
-
+    item.parentElement.parentElement.nextElementSibling.innerText = `${make} likes;`
+  }, { once: true });
+});
 })
-.then(getArr())
+.then(getArr());
+
+
+
+
+
+
+
 
 
 

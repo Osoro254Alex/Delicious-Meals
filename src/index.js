409b3a1556ea9143919cab886fe913ade1760d15
Alex Ogeto <alexogeto54@gmail.com>
2022-11-04 22:46:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import popData from './modules/popup.js';
 import foods from './modules/recordfood.js';
 
 const cardsContainer = document.querySelector('.card-cont');
-const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=k';
+const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=r';
 
 const involve = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/7wsNKEjPfzGMOmZ1AdMF/likes/';
 
